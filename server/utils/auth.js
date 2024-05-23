15a8bf3ab6715b739d484560615b367fd2774004
Elijah Matar <m.matar515@gmail.com>
2024-05-23 18:56:18 -0500

    removed uneeded query.token/

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -15,7 +15,7 @@ module.exports = {
   // function for our authenticated routes
   authMiddleware: function ({ req, res, next }) {
     // allows token to be sent via  req.query or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
+    let token = req.body.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -30,7 +30,7 @@ module.exports = {
     // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
+      req.profile = data;
     } catch {
       console.log("Invalid token");
       // return res.status(400).json({ message: 'invalid token!' });
