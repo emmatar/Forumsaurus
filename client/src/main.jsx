009733177c01021f9d1144453329d03e882ed1d2
Alex Gael <alexanderjgael@gmail.com>
2024-05-22 19:20:41 -0500

    modified default styling

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom/dist'
 import './index.css'
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
 import App from './App.jsx'
 import Forum from './pages/Forum';
