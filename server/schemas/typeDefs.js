07767f353d4f212ef517595bcbb12f1acf788519
Elijah Matar <m.matar515@gmail.com>
2024-05-23 21:01:55 -0500

    updated typdefs:
    Post: included some requirements.
    
    Comment: Included updated name (comment to commentBody)
    
    Mutations Args:
    edits to:
    - addpost
    - removePost
    - addComment
    - removeComment

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -11,19 +11,19 @@ const typeDefs = `
   scalar Date
 
   type Post {
-    _id: ID
-    title: String
-    content: String
+    _id: ID!
+    title: String!
+    content: String!
     date: Date
-    profile: Profile
+    profile: Profile!
     rawrs: Int
     comments: [Comment]
   }
 
   type Comment {
-    _id: ID
-    body: String
-    profile: Profile
+    _id: ID!
+    commentBody: String!
+    profile: Profile!
     post: Post
   }
 
@@ -40,14 +40,13 @@ const typeDefs = `
 
   type Mutation {
     addProfile(username: String!, email: String!, password: String!): Auth
-    updateProfile(username: String!, email: String!, password: String!): Auth
     removeProfile: Profile
     login(email: String!, password: String!): Auth
 
-    addPost(postId: ID!): Profile
-    removePost(postId: ID!): Profile
-    addComment(commentId: ID!): Post
-    removeComment(commentId: ID!): Post
+    addPost(title: String!, content: String!): Post
+    removePost(postId: ID!): Post
+    addComment(postId: ID!, commentBody: String!): Post
+    removeComment(postId: ID!, commentId: ID!): Post
   }
 `;
 
