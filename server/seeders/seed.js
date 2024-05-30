a361539b944e67ec04d624209fe7b092151e451c
Elijah Matar <m.matar515@gmail.com>
2024-05-29 19:26:40 -0500

    removed for-loop regarding created into profile model

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -8,20 +8,8 @@ db.once("open", async () => {
   try {
     await cleanDB("Profile", "profiles");
     await cleanDB("Post", "posts");
+    
 
-    await Profile.create(profileSeeds);
-
-    for (let i = 0; i < postSeeds.length; i++) {
-      const { _id, postAuthor } = await Post.create(postSeeds[i]);
-      const profile = await Profile.findOneAndUpdate(
-        { username: postAuthor },
-        {
-          $addToSet: {
-            posts: _id,
-          },
-        }
-      );
-    }
   } catch (err) {
     console.error(err);
     process.exit(1);
