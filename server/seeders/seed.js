816d8718335e73e5ccff6231b393c731ba54819d
Elijah Matar <m.matar515@gmail.com>
2024-05-29 21:07:59 -0500

    created logic for looping through seed data

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -2,17 +2,19 @@ const db = require("../config/connection");
 const { Profile, Post } = require("../models");
 const cleanDB = require("./cleanDB");
 const profileSeeds = require("./profileSeeds.json");
-const postSeeds = require("./postSeeds.json");
 
 db.once("open", async () => {
-  try {
-    await cleanDB("Profile", "profiles");
-    await cleanDB("Post", "posts");
-    
+  await cleanDB("Profile", "profiles");
+  await cleanDB("Post", "posts");
 
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
+
+  for (const { profile, posts } of profileSeeds) {
+    let postIds = [];
+    for (const post of posts) {
+      const { _id } = await Post.create(post);
+      postIds.push(_id);
+    }
+    await Profile.create({ ...profile, posts: postIds });
   }
 
   console.log("all done!");
